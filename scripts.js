58724eb7c8442f595d421b7703fba7cde64f9604
heyMP <mgp140@gmail.com>
2019-10-30 11:07:56 -0400

    Added Comments. 🔮

--- scripts.js
+++ scripts.js
@@ -9,19 +9,22 @@ class XCard extends LitElement {
           --x-card--top-color--background-color-default: var(--theme-accent-1);
           --x-card--text--color-default: var(--theme-font-color);
         }
-        [part="text"] {
-          color: var(--x-card--text--color);
-        }
         [part="top-color"] {
+          /* properties that are tied into the "--theme" convention define a css variable --[element-name]--[css-part-name]--[css-property] */
           background-color: var(--x-card--top-color--background-color, var(--x-card--top-color--background-color-default, gray));
+          /* properties that are NOT tied into the "--theme" convention define a css variable --[element-name]--[css-part-name]--[css-property], default value */
           height: var(--x-card--top-color--height, 10px);
           width: var(--x-card--top-color--width, 100%);
         }
+        [part="text"] {
+          color: var(--x-card--text--color, var(--x-card--text--color-default, gray));
+        }
       </style>
+      <!-- All elements get labeled with "part" attribute -->
       <div part="container">
         <div part="top-color"></div>
-        <div part="title">Title</div>
-        <div part="text">Blah blah blah...</div>
+        <div part="title"><slot name="title"></slot></div>
+        <div part="text"><slot name="text"></slot></div>
         <slot></slot>
       </div>
     `

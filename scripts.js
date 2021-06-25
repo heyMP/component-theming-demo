e4730219c9bd0bbcb5f3a0375ada2de6e614b731
heyMP <mgp140@gmail.com>
2021-06-25 09:26:54 -0400

    feat: updating demo to use private local variables

--- scripts.js
+++ scripts.js
@@ -4,33 +4,60 @@ class XCard extends LitElement {
   render() {
     return html`
       <style>
+        *, *:before, *:after {
+          box-sizing: border-box;
+        }
         :host {
           display: block;
-          /* DO NOT Target these variables that end in -local */
-          --x-card--top-color--background-color-local: var(--theme-accent-1);
-          --x-card--text--color-local: var(--theme-font-color);
+          /* DO NOT Target these variables that start with an underscore i.e. (--_) */
+          /* --[element-name]__[css-part-name]--[css-property] */
+          --_accentColor: var(--x-card--accentColor, var(--theme-accent-1, #ff9a00));
+          --_textColor: var(--x-card--textColor, var(--theme--text--color, #2e2e2e));
+          --_backgroundColor: var(--theme--surface--backgroundColor, #f5f5f5);
+          --_surfacePadding: var(--theme--surface--padding, 1em);
+          background-color: var(--_backgroundColor);
         }
         [part="top-color"] {
-          /* properties that are tied into the "--theme" convention define a css variable --[element-name]--[css-part-name]--[css-property] */
-          background-color: var(--x-card--top-color--background-color, var(--x-card--top-color--background-color-local, blue));
-          /* properties that are NOT tied into the "--theme" convention define a css variable --[element-name]--[css-part-name]--[css-property], default value */
-          height: var(--x-card--top-color--height, 10px);
-          width: var(--x-card--top-color--width, 100%);
+          background-color: var(--x-card__top-color--BackgroundColor, var(--_accentColor));
+          height: 10px;
+          width: 100%;
+        }
+        [part="title-container"] {
+          display: flex;
+          align-items: center;
+        }
+        [part="title"] {
+          color: var(--x-card__title--Color, var(--_accentColor));
+          padding: var(--_surfacePadding) var(--_surfacePadding) 0;
+          flex: 1 1 auto;
+        }
+        [part="title"] ::slotted(*) {
+          margin: 0;
+        }
+        [part="icon"] {
+          color: var(--x-card__icon--Color, var(--_accentColor));
+          padding: 0 var(--_surfacePadding);
+        }
+        [part="description"] {
+          color: var(--_textColor);
+          padding: calc(var(--_surfacePadding) / 4) var(--_surfacePadding) 0;
+          font-style: var(--x-card__description--FontStyle, italic);
         }
         [part="text"] {
-          color: var(--x-card--text--color, var(--x-card--text--color-local, green));
-        }
-        .title {
-          color: var(--x-card--title--color);
+          color: var(--_textColor);
+          padding: 0 calc(var(--_surfacePadding) * 2);
         }
       </style>
       <!-- All elements get labeled with "part" attribute -->
       <div part="container">
         <div part="top-color"></div>
-        <div part="title"><slot name="title"></slot></div>
-        <div part="text"><slot name="text"></slot></div>
-        <div part="slot">
-          <slot></slot>
+        <div part="title-container">
+          <div part="title"><slot name="title"></slot></div>
+          <div part="icon"><slot name="icon"></slot></div>
+        </div>
+        <div part="description"><slot name="description"></slot></div>
+        <div part="text">
+          <slot name="text"></slot>
         </div>
       </div>
     `
